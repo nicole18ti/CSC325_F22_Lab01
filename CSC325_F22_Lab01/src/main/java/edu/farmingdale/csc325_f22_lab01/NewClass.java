00c9354cd930e032df4d8af116d04c60ba6a91d4
nicolechan1118@gmail.com <nicolechan@localhost>
2022-08-31 10:26:54 -0400

    Add sayHello method with an iterator

--- CSC325_F22_Lab01/src/main/java/edu/farmingdale/csc325_f22_lab01/NewClass.java
+++ CSC325_F22_Lab01/src/main/java/edu/farmingdale/csc325_f22_lab01/NewClass.java
@@ -4,10 +4,30 @@ public class NewClass {
 
     private static void sayHello(String str) {
         System.out.println(str);
+
+    }
+
+    private static void sayHello(String str, int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(str);
+        }
+
     }
 
     public static void main(String[] args) {
 
-        sayHello("Helllo World");
+        sayHello("Helllo World", 5);
     }
+}
+
+class Class2 {
+
+    public Class2() {
+
+    }
+
+    public Class2(int x) {
+
+    }
+
 }
