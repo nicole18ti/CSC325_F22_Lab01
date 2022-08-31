df446660b47003f915098cf54da35df68c8fba9a
nicolechan1118@gmail.com <nicolechan@localhost>
2022-08-31 10:04:15 -0400

    Refeactor main method and add hello

--- CSC325_F22_Lab01/src/main/java/edu/farmingdale/csc325_f22_lab01/NewClass.java
+++ CSC325_F22_Lab01/src/main/java/edu/farmingdale/csc325_f22_lab01/NewClass.java
@@ -1,9 +1,13 @@
 package edu.farmingdale.csc325_f22_lab01;
 
-
 public class NewClass {
-    
+
+    private static void sayHello(String str) {
+        System.out.println(str);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        sayHello("Helllo World");
     }
 }
